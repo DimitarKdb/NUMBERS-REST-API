410ce94694854f93a4415afcb6c00ff04978cb95
Dimitar <mitko.kod@gmail.com>
2024-10-15 00:00:39 +0300

    Restructured ServerNIO class

--- src/project/api/rest/numbers/server/ServerNIO.java
+++ src/project/api/rest/numbers/server/ServerNIO.java
@@ -9,6 +9,7 @@ import project.api.rest.numbers.type.facts.FactType;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.channels.ClosedChannelException;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
@@ -29,25 +30,39 @@ public class ServerNIO {
     private static final int YEAR_PARAMETERS = 1;
     private static final int DATE_PARAMETERS = 2;
 
+    private static boolean hasClients;
+    private static int clientCount;
+
     private static final RequestHandler client = RequestHandler.getInstance();
 
     public static void main(String[] args) {
 
-        try (ServerSocketChannel serverSocketChannel = ServerSocketChannel.open()) {
+        establishServer();
 
-            serverSocketChannel.bind(new InetSocketAddress(SERVER_HOST, SERVER_PORT));
-            serverSocketChannel.configureBlocking(false);
+    }
 
-            Selector selector = Selector.open();
-            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+    private static void establishServer() {
+        try (ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+             Selector selector = Selector.open()) {
+
+            bindServerSocketChannel(serverSocketChannel);
+            registerSelector(selector, serverSocketChannel);
 
             ByteBuffer buffer = ByteBuffer.allocateDirect(BUFFER_SIZE);
 
-            boolean hasClients = true;
-            int clientCounter = 0;
+            clientCount = 0;
+            hasClients = true;
 
             System.out.println("Server has been set up!");
 
+            handleClients(selector, buffer);
+        } catch (IOException e) {
+            throw new RuntimeException("There was a problem with server communication!", e);
+        }
+    }
+
+    private static void handleClients(Selector selector, ByteBuffer buffer) {
+        try {
             while (hasClients) {
                 int readyChannels = selector.select();
                 if (readyChannels == 0) {
@@ -57,50 +72,17 @@ public class ServerNIO {
                 Set<SelectionKey> selectedKeys = selector.selectedKeys();
                 Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
 
-
                 while (keyIterator.hasNext()) {
                     SelectionKey key = keyIterator.next();
+
                     if (key.isReadable()) {
-                        SocketChannel socketChannel = (SocketChannel) key.channel();
 
-                        buffer.clear();
-                        int query = socketChannel.read(buffer);
-
-                        if (query < 0) {
-                            System.out.println("Client has closed the connection");
-                            socketChannel.close();
-                            clientCounter--;
-
-                            if(clientCounter == 0) {
-                                hasClients = false;
-                            }
-
-                            continue;
-                        }
-
-                        buffer.flip();
-
-                        byte[] byteArray = new byte[buffer.remaining()];
-                        buffer.get(byteArray);
-                        String input = new String(byteArray, STANDARD_CHARSET);
-
-                        Command command = Command.extractCommand(input);
-
-                        Result result = runCommand(command);
-
-                        buffer.clear();
-                        buffer.put(result.toString().getBytes());
-
-                        buffer.flip();
-                        socketChannel.write(buffer);
+                        serviceReadableKey(key, buffer);
 
                     } else if (key.isAcceptable()) {
-                        ServerSocketChannel sockChannel = (ServerSocketChannel) key.channel();
-                        SocketChannel accept = sockChannel.accept();
-                        accept.configureBlocking(false);
-                        accept.register(selector, SelectionKey.OP_READ);
-                        System.out.println("A new client has connected!");
-                        clientCounter++;
+
+                        serviceAcceptableKey(key, selector);
+
                     }
 
                     keyIterator.remove();
@@ -108,7 +90,79 @@ public class ServerNIO {
 
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("An error occurred while handling clients requests!", e);
+        }
+
+        System.out.println("No more clients, server shutting down...");
+    }
+
+    private static void bindServerSocketChannel(ServerSocketChannel serverSocketChannel) {
+        try {
+            serverSocketChannel.bind(new InetSocketAddress(SERVER_HOST, SERVER_PORT));
+            serverSocketChannel.configureBlocking(false);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not bind server socket channel!", e);
+        }
+    }
+
+    private static void registerSelector(Selector selector, ServerSocketChannel serverSocketChannel) {
+        try {
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+        } catch (ClosedChannelException e) {
+            throw new RuntimeException("Error while trying to register the server socket in the selector!", e);
+        }
+    }
+
+    private static void serviceReadableKey(SelectionKey key, ByteBuffer buffer) {
+        try {
+
+            SocketChannel socketChannel = (SocketChannel) key.channel();
+
+            buffer.clear();
+            int query = socketChannel.read(buffer);
+
+            if (query < 0) {
+                clientCount--;
+                System.out.println("Client " + socketChannel.getRemoteAddress() + " has closed the connection, clients remaining: " + clientCount + "!");
+                socketChannel.close();
+
+                if (clientCount == 0) {
+                    hasClients = false;
+                }
+
+                return;
+            }
+
+            buffer.flip();
+
+            byte[] byteArray = new byte[buffer.remaining()];
+            buffer.get(byteArray);
+            String input = new String(byteArray, STANDARD_CHARSET);
+
+            Command command = Command.extractCommand(input);
+
+            Result result = runCommand(command);
+
+            buffer.clear();
+            buffer.put(result.toString().getBytes());
+
+            buffer.flip();
+            socketChannel.write(buffer);
+        } catch (IOException e) {
+            throw new RuntimeException("An error occurred while trying to service a readable key!", e);
+        }
+    }
+
+    private static void serviceAcceptableKey(SelectionKey key, Selector selector) {
+        try {
+            ServerSocketChannel sockChannel = (ServerSocketChannel) key.channel();
+            SocketChannel accept = sockChannel.accept();
+            accept.configureBlocking(false);
+            accept.register(selector, SelectionKey.OP_READ);
+            clientCount++;
+            System.out.println("A new client " + accept.getRemoteAddress() + " has connected, clients connected: " + clientCount + "!");
+        } catch (IOException e) {
+            throw new RuntimeException("An error occurred while trying to service an acceptable key!", e);
         }
     }
 
@@ -185,7 +239,7 @@ public class ServerNIO {
             default -> FactType.TRIVIA;
         };
 
-        return client.apiResponse(new String[] {"random"}, type);
+        return client.apiResponse(new String[]{"random"}, type);
     }
 
 }
